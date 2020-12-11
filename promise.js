1d0af3549891c8008da5673bb828810ca6182d78
baijie <baijie@PPD-0302000386deMacBook-Pro.local>
2020-12-11 21:14:25 +0800

    promise

---
+++ promise.js
@@ -0,0 +1,44 @@
+
+const MyPromise = (function () {
+    const PENDING = 'pending'
+    const RESOLVED = 'resolved'
+    const REJECTED = 'rejected'
+
+    function F(fn) {
+        const that = this
+        this.state = PENDING
+        this.value = null
+        this.resolvedFnStack = []
+        this.rejectedFnStack = []
+        typeof fn === 'function' && fn(resolve, reject)
+        function resolve(v) {
+            if (that.state === PENDING) {
+                this.value = v
+                that.state = RESOLVED
+                that.resolvedFnStack.forEach(item => item(that.value))
+            }
+        }
+        function reject(v) {
+            if (that.state === PENDING) {
+                that.value = v
+                that.state = REJECTED
+                that.rejectedFnStack.forEach(item => item(that.value))
+            }
+        }
+    }
+    F.prototype.then = function (resolvedFn) {
+        if (typeof resolvedFn === 'function') {
+            if (this.state === RESOLVED) resolvedFn(this.value)
+            else this.resolvedFnStack.push(resolvedFn)
+        }
+        return this
+    }
+    F.prototype.catch = function (rejectedFn) {
+        if (typeof rejectedFn === 'function') {
+            if (this.state === REJECTED) rejectedFn(this.value)
+            else this.resolvedFnStack.push(rejectedFn)
+        }
+        return this;
+    }
+    return F
+})()
